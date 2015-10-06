3b058babe3f3c818d4cb3b027b1ee6c516ae2168
Fred Grott <fred.grott@gmail.com>
2015-10-06 10:15:55 -0500

    added library code

---
+++ library/src/main/java/com/github/shareme/gwsaction/library/action/CloseAction.java
@@ -0,0 +1,22 @@
+package com.github.shareme.gwsaction.library.action;
+
+/**
+ * CloseAction class
+ * Created by fgrott on 8/29/2015.
+ */
+@SuppressWarnings("unused")
+public class CloseAction extends Action {
+
+    public CloseAction() {
+        final float start = 0.239375f;
+        final float end = 1f - start;
+
+        lineData = new float[]{
+                // line 1
+                start, start, end, end,
+                // line 2
+                start, end, end, start,
+                // line 3
+                start, start, end, end,};
+    }
+}
---
+++ library/src/main/java/com/github/shareme/gwsaction/library/action/DrawerAction.java
@@ -0,0 +1,26 @@
+package com.github.shareme.gwsaction.library.action;
+
+/**
+ * DrawerAction class
+ * Created by fgrott on 8/29/2015.
+ */
+@SuppressWarnings("unused")
+public class DrawerAction extends Action {
+
+    public DrawerAction() {
+
+        final float startX = 0.1375f;
+        final float endX = 1f - startX;
+        final float endY = 0.707f;
+        final float startY = 1f - endY;
+        final float center = 0.5f;
+
+        lineData = new float[]{
+                // line 1
+                startX, startY, endX, startY,
+                // line 2
+                startX, center, endX, center,
+                // line 3
+                startX, endY, endX, endY,};
+    }
+}
